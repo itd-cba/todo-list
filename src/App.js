1dbfe6dacbdb473c1c0a4458e70c8fcb3105974c
Carsten Bartelt <carsten.bartelt@itdesign.de>
2021-08-09 12:20:29 +0200

    Implements Task adding functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React, {useState} from "react";
+import {nanoid} from "nanoid";
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 
 function App(props) {
-    const taskList = props.tasks.map(task => (
+    const [tasks, setTasks] = useState(props.tasks)
+    const taskList = tasks.map(task => (
             <Todo
                 id={task.id}
                 name={task.name}
@@ -13,14 +16,19 @@ function App(props) {
         )
     );
 
+    function addTask(name) {
+        const newTask = {id: "todo-" + nanoid(), name: name, completed: false};
+        setTasks([...tasks, newTask])
+    }
+
     return (
         <div className="todoapp stack-large">
             <h1>TodoMatic</h1>
-            <Form/>
+            <Form addTask={addTask}/>
             <div className="filters btn-group stack-exception">
-                <FilterButton />
-                <FilterButton />
-                <FilterButton />
+                <FilterButton/>
+                <FilterButton/>
+                <FilterButton/>
             </div>
             <h2 id="list-heading">
                 3 tasks remaining
