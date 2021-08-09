05775a34a305416f4ddb42452f297bb61b7465f8
Carsten Bartelt <carsten.bartelt@itdesign.de>
2021-08-09 17:07:40 +0200

    Implements filtering of tasks

--- src/App.js
+++ src/App.js
@@ -4,20 +4,39 @@ import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 
+const FILTER_MAP = {
+    All: () => true,
+    Active: task => !task.completed,
+    Completed: task => task.completed
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
     const [tasks, setTasks] = useState(props.tasks)
-    const taskList = tasks.map(task => (
-            <Todo
-                id={task.id}
-                name={task.name}
-                completed={task.completed}
-                key={task.id}
-                toggleTaskCompleted={toggleTaskCompleted}
-                deleteTask={deleteTask}
-                editTask={editTask}
-            />
-        )
-    );
+    const [filter, setFilter] = useState("All")
+    const filterList = FILTER_NAMES.map(name => (
+        <FilterButton
+            key={name}
+            name={name}
+            isPressed={name === filter}
+            setFilter={setFilter}
+        />
+    ));
+
+    const taskList = tasks
+        .filter(FILTER_MAP[filter])
+        .map(task => (
+                <Todo
+                    id={task.id}
+                    name={task.name}
+                    completed={task.completed}
+                    key={task.id}
+                    toggleTaskCompleted={toggleTaskCompleted}
+                    deleteTask={deleteTask}
+                    editTask={editTask}
+                />
+            )
+        );
 
     const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
     const headingText = `${taskList.length} ${tasksNoun} remaining`;
@@ -56,14 +75,13 @@ function App(props) {
         setTasks(editedTasks);
     }
 
+
     return (
         <div className="todoapp stack-large">
             <h1>TodoMatic</h1>
             <Form addTask={addTask}/>
             <div className="filters btn-group stack-exception">
-                <FilterButton/>
-                <FilterButton/>
-                <FilterButton/>
+                {filterList}
             </div>
             <h2 id="list-heading">
                 {headingText}
